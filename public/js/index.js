20c9368e01d796dc831e5b4c63f6be53cc08019b
Jeremias ramirez <jereramirez499@gmail.com>
2018-12-07 16:10:14 -0400

    maquetado completo

---
+++ public/js/index.js
@@ -0,0 +1,43 @@
+ 
+
+// clase de app task
+
+	class Task{
+
+		constructor(valor){
+			this.valor = valor;
+		}
+
+
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ let inputValue = document.getElementById("input__write-js");
+ let button = document.getElementById("button___send-js");
+
+ if(inputValue && button){
+
+ }
+
+
+
+
+
+
